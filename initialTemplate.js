41d02315714779ebc04a37eb175b0be831c2a4c0
jmhwang7 <jmhwang7@gmail.com>
2014-04-06 22:31:02 -0400

    initial commit

---
+++ initialTemplate.js
@@ -0,0 +1,32 @@
+/**
+ *  1. Define document sizes
+ */
+
+/**
+ *  2. Define scales and axes
+ *  7. (Later) Define category colors
+ */
+
+/**
+ *  3. Append SVG to place elements in.
+ */
+
+/**
+ *  4. Load data
+ */
+
+/**
+ *  5. Append axes with no labels
+ */
+
+/**
+ *  6. Append data elements to the data
+ */
+
+/**
+ *  7. Differentiate categories of data elements with color
+ */
+
+/**
+ *  8. Add legend
+ */
