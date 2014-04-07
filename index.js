e698b17032f48e913cd9ee820ae78ee15cce46b9
jmhwang7 <jmhwang7@gmail.com>
2014-04-06 23:38:40 -0400

    2. Scales and Axes Defined

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ var margin = {top: 20, right: 20, bottom: 30, left: 40},
  *  2. Define scales and axes
  *  7. (Later) Define category colors
  */
+// Scales map an input domain to an output range;
+// AKA maps values from your data to some pixel value,
+// because we're defining the range as the pixel values
+// encompassed by the main SVG element.
+//
+// We set the domain later when we load the data.
+var x = d3.scale.linear()
+  .range([0, width]); // x and y are functions
+var y = d3.scale.linear()
+  .range([height, 0]); // height to 0, because origin is top left corner
+
+// Axes are the reference lines for scales that are actually seen
+// on the screen. d3 handles labeling, spacing of ticks, etc. with
+// the axis function.
+var xAxis = d3.svg.axis()
+  .scale(x) // Pass in the scale the axis is based on
+  .orient("bottom");
+var yAxis = d3.svg.axis()
+  .scale(y)
+  .orient("left");
 
 /**
  *  3. Append SVG to place elements in.
